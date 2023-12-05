8ae50503497bb41cf9133df0757c53cd8b06388e
poliana-dev <m.polisys02@gmail.com>
2023-12-05 11:25:27 -0300

    reparos e acho que deu tudo certo (precisa de revisão)

--- paginas/views.py
+++ paginas/views.py
@@ -10,3 +10,6 @@ def setores_view(request):
 
 def divulgacoes_view(request):
     return render(request, 'divulgacoes.html')
+
+def tela_admin_view(request):
+    return render(request, 'tela-admin.html')
