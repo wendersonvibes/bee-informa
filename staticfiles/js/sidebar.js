bf14a8624fa42cb75223c33f38cfb81715bf1be2
poliana-dev <m.polisys02@gmail.com>
2023-12-09 12:41:01 -0300

    sidebar admin inicial está responsiva e atualizada
    falta apenas o conteudo a ser mostrado no dashboard :>

--- staticfiles/js/sidebar.js
+++ staticfiles/js/sidebar.js
@@ -2,3 +2,5 @@ const toggler = document.querySelector(".btn");
 toggler.addEventListener("click",function(){
     document.querySelector("#sidebar").classList.toggle("collapsed");
 });
+
+document.getElementById('sidebar')
