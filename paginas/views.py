97d4a61152f337519545b68ca135b9ef24258964
wenderson <110772284+wendersonvibes@users.noreply.github.com>
2023-12-13 08:41:04 -0300

    sidebar atualizada

--- paginas/views.py
+++ paginas/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from servicos.models import Divulgacao
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -12,4 +14,11 @@ def divulgacoes_view(request):
     return render(request, 'divulgacoes.html')
 
 def tela_admin_view(request):
-    return render(request, 'tela-admin.html')
+    divulgacoes = Divulgacao.objects.all().order_by('data_registro')
+    paginacao = Paginator(divulgacoes, 5)
+
+    page_number = request.GET.get("page")
+    page_obj = paginacao.get_page(page_number)
+
+    context = {'page_obj': page_obj}
+    return render(request, 'tela_admin.html', context=context)
